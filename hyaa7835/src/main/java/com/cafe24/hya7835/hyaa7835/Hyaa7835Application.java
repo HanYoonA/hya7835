db3f86a7c22cfbe825a7b8cea876cc14bfe49755
yoona misun <hya7835@naver.com>
2020-08-11 11:43:03 +0900

    [한윤아]포폴 업로드

---
+++ hyaa7835/src/main/java/com/cafe24/hya7835/hyaa7835/Hyaa7835Application.java
@@ -0,0 +1,13 @@
+package com.cafe24.hya7835.hyaa7835;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hyaa7835Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hyaa7835Application.class, args);
+	}
+
+}
---
+++ hyaa7835/src/test/java/com/cafe24/hya7835/hyaa7835/Hyaa7835ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe24.hya7835.hyaa7835;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hyaa7835ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
